302c4f0f89743aab8c8307e6f0702941befb8b5e
Deniyi <adeniyi.olaitanhector@yahoo.com>
2026-01-09 14:17:23 +0100

    added next auth session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   },
 };
 
+import { Providers } from "./providers";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
