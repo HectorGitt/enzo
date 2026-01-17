0624ee700524116087bbc7ef641407dc9e2fc934
Deniyi <adeniyi.olaitanhector@yahoo.com>
2026-01-17 18:39:29 +0100

    feat: add Google Analytics integration for improved tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,19 @@ export default function RootLayout({
 					})(window,document,'script','dataLayer','GTM-WFG87KQB');
 					`}
 				</Script>
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=G-573XER5P4G"
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-573XER5P4G');
+					`}
+				</Script>
 			</head>
 			<body>
 				<noscript>
